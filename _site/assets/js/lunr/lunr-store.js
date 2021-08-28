046dd902884bd69b7cb586ec62872e4077ba405d
frog <frog@falinux.com>
2021-08-29 07:38:57 +0900

    backup

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "첫번째 글!",
+        "excerpt":"이야~, 글 첨 써 보네.   워치 옵션까지 적당히 잘 동작하는데…   아래 글 괜찮다.   https://devinlife.com/howto/   GitHub Pages 블로그 따라하기  ","categories": [],
+        "tags": [],
+        "url": "/%EC%B2%AB%EB%B2%88%EC%A7%B8-%EA%B8%B0%EB%A1%9D/",
+        "teaser": null
+      }]
